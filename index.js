ec37811d4ed57909bb1d429ee0097b12e81f77e3
carleandro7 <carleandro7@gmail.com>
2023-09-08 07:42:52 -0300

    adicionado post relacionado com user

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const conn = require('./db/database')
+const Handlebars = require('handlebars');
 
 const userRoutes = require('./routes/usersRoutes')
+const postsRoutes = require('./routes/postsRoutes')
 
 const app = express()
 const port = 3000
@@ -15,9 +17,19 @@ app.use(
         extended: true
     })
 )
+
+
+Handlebars.registerHelper('isEqual', function (a, b, options) {
+  if (a === b) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
 app.use(express.json())
 
 app.use('/users', userRoutes)
+app.use('/posts', postsRoutes)
 
 
 app.listen(port)
